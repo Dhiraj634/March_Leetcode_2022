611480f423c5473dd5da889f0b065994725c89d1
Dhiraj Kumar Yadav <dhiru634@gmail.com>
2022-03-04 11:10:52 +0530

    Added Dp solution for Champagne_Tower

--- src/Champagne_Tower.java
+++ src/Champagne_Tower.java
@@ -6,6 +6,32 @@ public class Champagne_Tower {
         int n = (query_row * (query_row+1)) / 2 + query_glass;
         get(query_row, poured, n);
         return map.get(n) >=1 ? 1 : map.get(n);
+//        return dpSolution(poured,query_row,query_glass);
+
+
+    }
+    private double dpSolution(int poured, int query_row, int query_glass){
+        double[][] dp = new double[100][];
+        for(int i=0;i<100;i++){
+            dp[i] = new double[i+1];
+        }
+        dp[0][0] = poured;
+        for(int i=1;i<100;i++){
+            boolean needToBreak = false;
+            for(int j=0;j<dp[i].length;j++){
+                if(j == 0){
+                    dp[i][j]+= dp[i-1][j] >=1 ? (dp[i-1][j] - 1) /2 : 0;
+                }else if(j == i){
+                    dp[i][j]+= dp[i-1][j-1] >=1 ? (dp[i-1][j-1] - 1) /2 : 0;
+                }else{
+                    dp[i][j]+= (dp[i-1][j-1] >=1 ? (dp[i-1][j-1] - 1) /2 : 0)  + (dp[i-1][j] >=1 ? (dp[i-1][j] - 1) /2 : 0);
+                }
+                needToBreak = (j ==query_glass && i == query_row);
+            }
+            if(needToBreak) break;
+
+        }
+        return dp[query_row][query_glass] >=1 ? 1 : dp[query_row][query_glass];
     }
     private double get(int row, int poured, int n){
         if(row == 0){
